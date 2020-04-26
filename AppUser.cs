14cb8112778852b1881086babd04700703e56d5d
JessicaRdz20 <60240227+JessicaRdz20@users.noreply.github.com>
2020-04-25 19:23:30 -0500

    Add files via upload

---
+++ AppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final_Project_Group_9.DAL
+{
+    public class AppUser : IdentityUser
+    {
+        //Identity creates several of the important fields for you
+        //TODO: Put any additional fields that you need for your user here (You don't need this for HW4)
+        //First name is provided as an example
+        [Required(ErrorMessage = "First name is required.")]
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+    }
+}
